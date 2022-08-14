9daa8fd00b0dbdb7fc5d687e02f4f70ef4b7dba9
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-08-14 23:39:51 +0300

    Without localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,40 @@
-import { useState, useEffect } from "react";
+import {useEffect } from "react";
 
 import { nanoid } from 'nanoid'
 import { ContactForm } from "./ContactForm/ContactForm"
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
+import { useSelector, useDispatch } from "react-redux";
+import {addItem, removeItem, filterValue} from "../reduxStore/actions"
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
+
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
 
   const visibleContacts = contacts
   .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
-  useEffect(() => {
-    const KEY = "contacts"
-    const savedData = localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : [];
+  // useEffect(() => {
+  //   const KEY = "contacts"
+  //   const savedData = localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : [];
 
-    if (savedData.length > 0) {
-      setContacts(savedData)
-    }
-  }, []);
+  //   if (savedData.length > 0) {
+  //     setContacts(savedData)
+  //   }
+  // }, []); 
 
-  useEffect(() => {
-    const KEY = "contacts";
-    const dataToSave = JSON.stringify(contacts);
+  // useEffect(() => {
+  //   const KEY = "contacts";
+  //   const dataToSave = JSON.stringify(contacts);
 
-    localStorage.setItem(KEY, dataToSave)
-  }, [contacts]);
+  //   localStorage.setItem(KEY, dataToSave)
+  // }, [contacts]);
 
     function onInputChange(e) {
       const { value } = e.target;
-      setFilter(value)
+      dispatch(filterValue(value))
     }
 
     function addContact(name, number) {
@@ -38,22 +42,16 @@ export const App = () => {
         alert(`${name} is already in contacts`)
         return
       } else {
-        setContacts(contacts => {
-        return (
-          [...contacts, {
+        dispatch(addItem({
             id: nanoid(),
             name: name,
             number: number,
-        }])
-      })
+        }));
       }
     }
 
     function deleteContact(id) {
-      setContacts(contacts => {
-        const newCotnacts = contacts.filter(contact => contact.id !== id);
-        return (newCotnacts)
-      })
+      dispatch(removeItem(id))
     }
   
   return (
