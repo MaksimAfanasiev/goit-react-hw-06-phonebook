1ee3212a81f74b3786a3640e7267b63b1b01bc11
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-08-15 00:28:47 +0300

    Finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import {useEffect } from "react";
-
 import { nanoid } from 'nanoid'
 import { ContactForm } from "./ContactForm/ContactForm"
 import { ContactList } from "./ContactList/ContactList";
@@ -8,7 +6,6 @@ import { useSelector, useDispatch } from "react-redux";
 import {addItem, removeItem, filterValue} from "../reduxStore/actions"
 
 export const App = () => {
-
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.items);
   const filter = useSelector(state => state.contacts.filter);
@@ -16,43 +13,27 @@ export const App = () => {
   const visibleContacts = contacts
   .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
-  // useEffect(() => {
-  //   const KEY = "contacts"
-  //   const savedData = localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : [];
+  function onInputChange(e) {
+    const { value } = e.target;
+    dispatch(filterValue(value))
+  }
 
-  //   if (savedData.length > 0) {
-  //     setContacts(savedData)
-  //   }
-  // }, []); 
-
-  // useEffect(() => {
-  //   const KEY = "contacts";
-  //   const dataToSave = JSON.stringify(contacts);
-
-  //   localStorage.setItem(KEY, dataToSave)
-  // }, [contacts]);
-
-    function onInputChange(e) {
-      const { value } = e.target;
-      dispatch(filterValue(value))
+  function addContact(name, number) {
+    if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts`)
+      return
+    } else {
+      dispatch(addItem({
+          id: nanoid(),
+          name: name,
+          number: number,
+      }));
     }
+  }
 
-    function addContact(name, number) {
-      if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
-        alert(`${name} is already in contacts`)
-        return
-      } else {
-        dispatch(addItem({
-            id: nanoid(),
-            name: name,
-            number: number,
-        }));
-      }
-    }
-
-    function deleteContact(id) {
-      dispatch(removeItem(id))
-    }
+  function deleteContact(id) {
+    dispatch(removeItem(id))
+  }
   
   return (
     <div>
